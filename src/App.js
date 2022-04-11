de29d8b61594e80d19ecc205b7af5bff09513452
Mrudulau30 <mrudulau30@gmail.com>
2022-04-11 22:27:05 +0530

    two factor auth

--- src/App.js
+++ src/App.js
@@ -12,265 +12,279 @@ import Coe from './components/Users/CoeDB';
 import Teacher from './components/Users/TeacherDB';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SuperDB from './components/Users/SuperDB';
+import Two_Factor from './components/Layout/Two_Factor';
 
 const ipfsClient = require('ipfs-http-client');
 const ipfs = ipfsClient.create({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https',
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
 });
 
 const App = () => {
-  const [dstorage, setDstorage] = useState(null);
-  const [networkId, setNetworkId] = useState(null);
-  const [account, setAccount] = useState(null);
-  const [files, setFiles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [capturedFileBuffer, setCapturedFileBuffer] = useState(null);
-  const [capturedFileType, setCapturedFileType] = useState(null);
-  const [capturedFileName, setCapturedFileName] = useState(null);
+        const [dstorage, setDstorage] = useState(null);
+        const [networkId, setNetworkId] = useState(null);
+        const [account, setAccount] = useState(null);
+        const [files, setFiles] = useState([]);
+        const [isLoading, setIsLoading] = useState(true);
+        const [capturedFileBuffer, setCapturedFileBuffer] = useState(null);
+        const [capturedFileType, setCapturedFileType] = useState(null);
+        const [capturedFileName, setCapturedFileName] = useState(null);
 
-  useEffect(() => {
-    // Check if the user has Metamask active
-    if (!web3) {
-      window.alert(
-        'Non-Ethereum browser detected. You should consider trying MetaMask!'
-      );
-      return;
-    }
+        useEffect(() => {
+            // Check if the user has Metamask active
+            if (!web3) {
+                window.alert(
+                    'Non-Ethereum browser detected. You should consider trying MetaMask!'
+                );
+                return;
+            }
 
-    // Function to fetch all the blockchain data
-    const loadBlockchainData = async () => {
-      // Request accounts acccess if needed
-      try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-      } catch (error) {
-        console.error(error);
-      }
+            // Function to fetch all the blockchain data
+            const loadBlockchainData = async() => {
+                // Request accounts acccess if needed
+                try {
+                    await window.ethereum.request({ method: 'eth_requestAccounts' });
+                } catch (error) {
+                    console.error(error);
+                }
 
-      // Load account
-      const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
+                // Load account
+                const accounts = await web3.eth.getAccounts();
+                setAccount(accounts[0]);
 
-      // Network ID
-      const networkId = await web3.eth.net.getId();
-      setNetworkId(networkId);
+                // Network ID
+                const networkId = await web3.eth.net.getId();
+                setNetworkId(networkId);
 
-      // Contract
-      const contract = getDStorage(networkId);
-      if (contract) {
-        // Set contract in state
-        setDstorage(contract);
-        // Get files amount
-        const filesCount = await contract.methods.fileCount().call();
-        // Load files&sort by the newest
-        for (let i = filesCount; i >= 1; i--) {
-          const file = await contract.methods.files(i).call();
-          setFiles((prevState) => {
-            return [...prevState, file];
-          });
-        }
+                // Contract
+                const contract = getDStorage(networkId);
+                if (contract) {
+                    // Set contract in state
+                    setDstorage(contract);
+                    // Get files amount
+                    const filesCount = await contract.methods.fileCount().call();
+                    // Load files&sort by the newest
+                    for (let i = filesCount; i >= 1; i--) {
+                        const file = await contract.methods.files(i).call();
+                        setFiles((prevState) => {
+                            return [...prevState, file];
+                        });
+                    }
 
-        setIsLoading(false);
+                    setIsLoading(false);
 
-        // Event subscription to File Uploaded
-        contract.events
-          .FileUploaded()
-          .on('data', (event) => {
-            const file = event.returnValues;
-            setFiles((prevState) => {
-              return [file, ...prevState];
+                    // Event subscription to File Uploaded
+                    contract.events
+                        .FileUploaded()
+                        .on('data', (event) => {
+                            const file = event.returnValues;
+                            setFiles((prevState) => {
+                                return [file, ...prevState];
+                            });
+                            setIsLoading(false);
+                        })
+                        .on('error', (error) => {
+                            console.log(error);
+                        });
+                } else {
+                    window.alert('DStorage contract not deployed to detected network.');
+                }
+            };
+
+            loadBlockchainData();
+
+            // Metamask Event Subscription - Account changed
+            window.ethereum.on('accountsChanged', (accounts) => {
+                setAccount(accounts[0]);
             });
-            setIsLoading(false);
-          })
-          .on('error', (error) => {
-            console.log(error);
-          });
-      } else {
-        window.alert('DStorage contract not deployed to detected network.');
-      }
-    };
 
-    loadBlockchainData();
+            // Metamask Event Subscription - Network changed
+            window.ethereum.on('chainChanged', (chainId) => {
+                window.location.reload();
+            });
+        }, []);
 
-    // Metamask Event Subscription - Account changed
-    window.ethereum.on('accountsChanged', (accounts) => {
-      setAccount(accounts[0]);
-    });
+        // Get file from user
+        const captureFile = (event) => {
+            event.preventDefault();
 
-    // Metamask Event Subscription - Network changed
-    window.ethereum.on('chainChanged', (chainId) => {
-      window.location.reload();
-    });
-  }, []);
+            const file = event.target.files[0];
 
-  // Get file from user
-  const captureFile = (event) => {
-    event.preventDefault();
+            const reader = new window.FileReader();
+            reader.readAsArrayBuffer(file);
+            reader.onloadend = () => {
+                setCapturedFileBuffer(Buffer(reader.result));
+                setCapturedFileType(file.type);
+                setCapturedFileName(file.name);
+            };
+        };
 
-    const file = event.target.files[0];
+        // Upload file to IPFS and push to the blockchain
+        const uploadFile = async(description) => {
+            // Add file to the IPFS
+            const fileAdded = await ipfs.add(capturedFileBuffer);
+            if (!fileAdded) {
+                console.error('Something went wrong when updloading the file');
+                return;
+            }
 
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onloadend = () => {
-      setCapturedFileBuffer(Buffer(reader.result));
-      setCapturedFileType(file.type);
-      setCapturedFileName(file.name);
-    };
-  };
+            // Assign value for the file without extension
+            if (capturedFileType === '') {
+                setCapturedFileType('none');
+            }
 
-  // Upload file to IPFS and push to the blockchain
-  const uploadFile = async (description) => {
-    // Add file to the IPFS
-    const fileAdded = await ipfs.add(capturedFileBuffer);
-    if (!fileAdded) {
-      console.error('Something went wrong when updloading the file');
-      return;
-    }
+            dstorage.methods
+                .uploadFile(
+                    fileAdded.cid.string,
+                    fileAdded.size,
+                    capturedFileType,
+                    capturedFileName,
+                    description
+                )
+                .send({ from: account })
+                .on('transactionHash', (hash) => {
+                    setIsLoading(true);
+                    setCapturedFileType(null);
+                    setCapturedFileName(null);
+                })
+                .on('error', (e) => {
+                    window.alert('Something went wrong when pushing to the blockchain');
+                    setIsLoading(false);
+                });
+        };
 
-    // Assign value for the file without extension
-    if (capturedFileType === '') {
-      setCapturedFileType('none');
-    }
+        const showContent = web3 && account && dstorage;
 
-    dstorage.methods
-      .uploadFile(
-        fileAdded.cid.string,
-        fileAdded.size,
-        capturedFileType,
-        capturedFileName,
-        description
-      )
-      .send({ from: account })
-      .on('transactionHash', (hash) => {
-        setIsLoading(true);
-        setCapturedFileType(null);
-        setCapturedFileName(null);
-      })
-      .on('error', (e) => {
-        window.alert('Something went wrong when pushing to the blockchain');
-        setIsLoading(false);
-      });
-  };
-
-  const showContent = web3 && account && dstorage;
-
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <React.Fragment>
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route
-              exact
-              path="/admin"
-              element={
-                <>
-                  <Navbar
-                    account={account}
-                    web3={web3}
-                    networkId={networkId}
-                    setAccount={setAccount}
-                  />
-                  <img
-                    src={box}
-                    className="mx-auto mt-3 rounded d-block"
-                    width="120"
-                    height="120"
-                    alt="logo"
-                  />
-                  {showContent && (
-                    <Main
-                      files={files}
-                      captureFile={captureFile}
-                      uploadFile={uploadFile}
-                      isLoading={isLoading}
+        return ( <
+            div >
+            <
+            BrowserRouter >
+            <
+            Routes >
+            <
+            React.Fragment >
+            <
+            Route exact path = "/dashboard"
+            element = { < Dashboard / > }
+            /> <
+            Route exact path = "/admin"
+            element = { <
+                >
+                <
+                Navbar
+                account = { account }
+                web3 = { web3 }
+                networkId = { networkId }
+                setAccount = { setAccount }
+                /> <
+                img
+                src = { box }
+                className = "mx-auto mt-3 rounded d-block"
+                width = "120"
+                height = "120"
+                alt = "logo" /
+                > {
+                    showContent && ( <
+                        Main files = { files }
+                        captureFile = { captureFile }
+                        uploadFile = { uploadFile }
+                        isLoading = { isLoading }
+                        />
+                    )
+                } <
+                />
+            }
+            /> <
+            Route exact path = "/teacher"
+            element = { <
+                >
+                <
+                Navbar
+                account = { account }
+                web3 = { web3 }
+                networkId = { networkId }
+                setAccount = { setAccount }
+                /> <
+                img
+                src = { box }
+                className = "mx-auto mt-3 rounded d-block"
+                width = "120"
+                height = "120"
+                alt = "logo" /
+                > {
+                    showContent && (
+                        // <Teacher
+                        //   files={files}
+                        //   captureFile={captureFile}
+                        //   uploadFile={uploadFile}
+                        //   isLoading={isLoading}
+                        // />
+                        <
+                        Two_Factor / >
+                    )
+                } <
+                />
+            }
+            /> <
+            Route exact path = "/coe"
+            element = { <
+                >
+                <
+                Navbar
+                account = { account }
+                web3 = { web3 }
+                networkId = { networkId }
+                setAccount = { setAccount }
+                /> <
+                img
+                src = { box }
+                className = "mx-auto mt-3 rounded d-block"
+                width = "120"
+                height = "120"
+                alt = "logo" /
+                > {
+                    showContent && < Coe files = { files }
+                    />} <
                     />
-                  )}
-                </>
-              }
-            />
-            <Route
-              exact
-              path="/teacher"
-              element={
-                <>
-                  <Navbar
-                    account={account}
-                    web3={web3}
-                    networkId={networkId}
-                    setAccount={setAccount}
-                  />
-                  <img
-                    src={box}
-                    className="mx-auto mt-3 rounded d-block"
-                    width="120"
-                    height="120"
-                    alt="logo"
-                  />
-                  {showContent && (
-                    <Teacher
-                      files={files}
-                      captureFile={captureFile}
-                      uploadFile={uploadFile}
-                      isLoading={isLoading}
+                }
+                /> <
+                Route
+                exact
+                path = "/super"
+                element = { <
+                    >
+                    <
+                    Navbar
+                    account = { account }
+                    web3 = { web3 }
+                    networkId = { networkId }
+                    setAccount = { setAccount }
+                    /> <
+                    img
+                    src = { box }
+                    className = "mx-auto mt-3 rounded d-block"
+                    width = "120"
+                    height = "120"
+                    alt = "logo" /
+                    > { showContent && < SuperDB / > } <
                     />
-                  )}
-                </>
-              }
-            />
-            <Route
-              exact
-              path="/coe"
-              element={
-                <>
-                  <Navbar
-                    account={account}
-                    web3={web3}
-                    networkId={networkId}
-                    setAccount={setAccount}
-                  />
-                  <img
-                    src={box}
-                    className="mx-auto mt-3 rounded d-block"
-                    width="120"
-                    height="120"
-                    alt="logo"
-                  />
-                  {showContent && <Coe files={files} />}
-                </>
-              }
-            />
-            <Route
-              exact
-              path="/super"
-              element={
-                <>
-                  <Navbar
-                    account={account}
-                    web3={web3}
-                    networkId={networkId}
-                    setAccount={setAccount}
-                  />
-                  <img
-                    src={box}
-                    className="mx-auto mt-3 rounded d-block"
-                    width="120"
-                    height="120"
-                    alt="logo"
-                  />
-                  {showContent && <SuperDB />}
-                </>
-              }
-            />
-          </React.Fragment>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
+                }
+                /> <
+                /React.Fragment> <
+                Route path = "/signup"
+                element = { < SignUp / > }
+                /> <
+                Route path = "/profile"
+                element = { < Profile / > }
+                /> <
+                Route path = "/login"
+                element = { < Login / > }
+                /> <
+                /Routes> <
+                /BrowserRouter> <
+                /div>
+            );
+        };
 
-export default App;
+        export default App;
---
+++ src/components/Layout/Two_Factor.jsx
@@ -0,0 +1,85 @@
+
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+import { signInWithPhoneNumber } from "firebase/auth";
+
+const handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+        [name]: value
+    })
+}
+const configureCaptcha = () => {
+    const auth = getAuth();
+    window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+        'size': 'invisible',
+        'callback': (response) => {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            this.onSignInSubmit();
+            console.log("Recaptcha verified")
+        },
+        defaultCountry: "IN"
+    }, auth);
+}
+
+const onSignInSubmit = (e) => {
+    e.preventDefault()
+
+    this.configureCaptcha()
+    const phoneNumber = "+91" + this.state.mobile
+    console.log(phoneNumber)
+
+    const appVerifier = window.recaptchaVerifier;
+
+    const auth = getAuth();
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+            // SMS sent. Prompt user to type the code from the message, then sign the
+            // user in with confirmationResult.confirm(code).
+            window.confirmationResult = confirmationResult;
+            console.log("OTP has been sent")
+                // ...
+        }).catch((error) => {
+            // Error; SMS not sent
+            // ...
+            console.log("sms nahi aaya")
+        });
+
+}
+ const  Two_Factor =()=>{
+  
+
+    const onSubmitOTP = (e) => {
+        e.preventDefault()
+        const code = this.state.otp
+        console.log(code)
+        window.confirmationResult.confirm(code).then((result) => {
+            // User signed in successfully.
+            const user = result.user;
+            console.log(JSON.stringify(user))
+            alert("USER IS VERIFIED")
+                // ...
+        }).catch((error) => {
+            // User couldn't sign in (bad verification code?)
+            // ...
+            console.log("Galat OTP hai yaaar")
+        });
+    }
+    return(
+        <div>
+        <h2>User Phone Number</h2>
+        <form onSubmit={this.onSignInSubmit}>
+          <div id="sign-in-button"></div>
+        <input type="number" name="mobile" placeholder="Mobile Number" required onChange={this.handleChange} />
+        <button type="submit"> Submit</button>
+         </form>
+         <h2>Enter OTP</h2>
+         <form onSubmit={this.onSubmitOTP}>
+        <input type="number" name="otp" placeholder="Enter OTP" required  onChange={this.handleChange}  />
+        <button type="submit"> Submit</button>
+         </form>
+     </div>
+    );
+};
+
+
+export default Two_Factor;
