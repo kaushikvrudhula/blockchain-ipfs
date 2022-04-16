f54e0b7aea4c4edf78b5bba60c7b566b7c357ae4
Ritik947 <gourritikkailash@gmail.com>
2022-04-16 22:18:43 +0530

    super page done

--- src/components/Users/SuperDB.jsx
+++ src/components/Users/SuperDB.jsx
@@ -2,33 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../Firebase';
 import { Button } from 'react-bootstrap';
+import pdfobject from 'pdfobject';
 
 function SuperDB() {
   const [hashcode, setHashcode] = useState(null);
-  async function getHash() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getHash = async () => {
     const docSnap = await getDoc(doc(db, 'Files', 'paper'));
-    if (docSnap) {
-      setHashcode(docSnap.data().hash);
-    } else {
-      console.log('No such document!');
-    }
-  }
+    const data = await docSnap.data();
+    const hash = await data.hash;
+    return hash;
+  };
   useEffect(() => {
-    getHash();
-  }, []);
+    let hash;
+    (async () => {
+      hash = await getHash();
+      console.log(hash);
+      setHashcode(hash);
+      setIsLoading(false);
+      pdfobject.embed(`https://ipfs.infura.io/ipfs/${hashcode}`, '#docdiv');
+    })();
+  }, [hashcode]);
   return (
     <div>
       <div class="text-center w-100 text-dark">
-        <button variant="light" className="fill">
-          <a
-            className="viewlink"
-            href={'https://ipfs.infura.io/ipfs/' + hashcode}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            View Paper Here{/* .substring(0, 10)}... */}
-          </a>
-        </button>
+        {isLoading === false ? (
+          <div id="docdiv"></div>
+        ) : (
+          <div>Fetching document</div>
+        )}
       </div>
     </div>
   );
